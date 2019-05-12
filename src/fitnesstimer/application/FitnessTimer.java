3d1c5565a36a38cc72e60e10219e3c82db561e9b
SaltyKawaiiNeko <software@skneko.moe>
2019-05-12 16:31:43 +0200

    Initial commit

---
+++ src/fitnesstimer/application/FitnessTimer.java
@@ -0,0 +1,32 @@
+/*
+ * IPC - Entrega 2
+ * Ingeniería Informática, UPV 2019
+ * Por:
+ *  Daniel Galán Pascual
+ *  Alberto Baixauli Herráez
+ */
+package fitnesstimer.application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class FitnessTimer extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fitnesstimer/views/TimeDashboard.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ src/fitnesstimer/controllers/TimeDashboardController.java
@@ -0,0 +1,42 @@
+/*
+ * IPC - Entrega 2
+ * Ingeniería Informática, UPV 2019
+ * Por:
+ *  Daniel Galán Pascual
+ *  Alberto Baixauli Herráez
+ */
+package fitnesstimer.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author Dani
+ */
+public class TimeDashboardController implements Initializable {
+
+    @FXML
+    private Button button;
+    @FXML
+    private Label label;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+    }
+    
+}
