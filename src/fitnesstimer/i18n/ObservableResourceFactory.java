5cb467b9b4d42348ffcae166f7c6ed1c285749c1
SaltyKawaiiNeko <software@skneko.moe>
2019-05-15 00:36:26 +0200

    Improve internationalization helper

--- src/fitnesstimer/i18n/ObservableResourceFactory.java
+++ src/fitnesstimer/i18n/ObservableResourceFactory.java
@@ -11,6 +11,7 @@ import java.util.ResourceBundle;
 import javafx.beans.binding.StringBinding;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.scene.control.Labeled;
 
 /**
  *
@@ -32,15 +33,23 @@ public class ObservableResourceFactory {
         resourcesProperty().set(resources);
     }
     
-    public StringBinding getStringBinding(String key) {
+    public StringBinding getStringBinding(String key, Object... args) {
         return new StringBinding() {
         
             { bind(resourcesProperty()); }
     
             @Override
             public String computeValue() {
-                return getResources().getString(key);
+                return getString(key, args);
             }
         };
+    }
+    
+    private String getString(String key, Object... args) {
+        return String.format(getResources().getString(key), args);
+    }
+    
+    public void bind(Labeled property, String key, Object... args) {
+        property.textProperty().bind(getStringBinding(key, args));
     }
 }
