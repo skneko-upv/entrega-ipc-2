bd9044217b08e4eebc1e7edc63bda3948619345d
SaltyKawaiiNeko <software@skneko.moe>
2019-06-07 18:02:16 +0200

    Add style change

--- src/fitnesstimer/controllers/base/AbstractController.java
+++ src/fitnesstimer/controllers/base/AbstractController.java
@@ -11,6 +11,7 @@ import accesoBD.AccesoBD;
 import fitnesstimer.i18n.I18n;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -26,22 +27,22 @@ import javafx.stage.Stage;
  * @author Dani
  */
 public abstract class AbstractController implements Initializable {
-    
+
     private enum Style {
         DAY, NIGHT
     }
     private static Style currentStyle = Style.DAY;
-    
+
     @FXML
     protected RadioMenuItem dayStyle;
     @FXML
     protected RadioMenuItem nightStyle;
     @FXML
     protected ToggleGroup style;
-    
+
     protected final AccesoBD db = AccesoBD.getInstance();
     protected final I18n i18n = I18n.getInstance();
-    
+
     /**
      * Initializes the controller class.
      * @param url Not used
@@ -49,28 +50,40 @@ public abstract class AbstractController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        applyStyle();
         if (style != null) {
             style.selectedToggleProperty().addListener((_val, _oldVal, selected) -> changeStyle(selected));
         }
+        Platform.runLater(() -> applyStyle());
     }
-    
+
     protected void close(ActionEvent event) {
         Node source = (Node) event.getSource();
         Stage stage = (Stage) source.getScene().getWindow();
         stage.close();
     }
-    
+
     @FXML
     protected void changeStyle(Toggle toggle) {
-        // TODO
+        if (style.getSelectedToggle() == null || style.getSelectedToggle().equals(dayStyle)) {
+            currentStyle = Style.DAY;
+        } else if (style.getSelectedToggle().equals(nightStyle)) {
+            currentStyle = Style.NIGHT;
+        }
+        applyStyle();
     }
-    
+
     @FXML
     protected void applyStyle() {
-        // TODO
+        switch (currentStyle) {
+            case NIGHT:
+                Application.setUserAgentStylesheet("fitnesstimer/resources/css/styleN.css");
+                break;
+            case DAY:
+            default:
+                Application.setUserAgentStylesheet("fitnesstimer/resources/css/style.css");
+        }
     }
-    
+
     @FXML
     private void onQuit(ActionEvent event) {
         Platform.exit();
